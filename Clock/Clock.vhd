e6ab1572de002f36fbd08c6997bd0faa4183f167
Konstantin K <k.korzhanevskii@g.nsu.ru>
2023-10-24 13:39:19 +0700

    Prescaalers are in new file now

--- Clock/Clock.vhd
+++ Clock/Clock.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
 
-use work.Math_PACK.all;
+--use work.Math_PACK.all;
 
 
 entity Clock is
@@ -22,66 +22,45 @@ end entity;
 
 
 architecture rtl of Clock is
-	constant CLOCK_PRESCALER 	: natural := (50000000);
-	signal 	 presc_reg			: std_logic_vector(f_log2(CLOCK_PRESCALER)-1 downto 0);
-	
-	signal	 sec_clk			: std_logic := '0';
-	signal   min_presc			: integer range 0 to 60 := 0;
-	signal	 min_clk			: std_logic := '0';
+	signal	 sec_clk_sig		: std_logic := '0';
+	signal	 min_clk_sig		: std_logic := '0';
 	
 	signal	 cur_time			: std_logic_vector(15 downto 0);
 	
-	component FourDigitDisplay is
+	component Prescalers is
 		port (
-			FourDigitDisplay_bcd	:	in std_logic_vector(15 downto 0);
-			FourDigitDisplay_segm	: 	out std_logic_vector(27 downto 0)
+			clk_i		:	in std_logic;
+			clk_sec_o	:	out std_logic;	
+			clk_min_o	:	out std_logic
 		);
-	end component; -- FourDigitDisplay
+	end component; -- Prescalers
 	
 	component ClockCore is
 		port (
 			ClockCore_clk			:	in  std_logic;
 			ClockCore_time_o		:	out std_logic_vector(15 downto 0)
 		);
-	end component; -- FourDigitDisplay
+	end component; -- ClockCore
 	
+	component FourDigitDisplay is
+		port (
+			FourDigitDisplay_bcd	:	in std_logic_vector(15 downto 0);
+			FourDigitDisplay_segm	: 	out std_logic_vector(27 downto 0)
+		);
+	end component; -- FourDigitDisplay	
 	
 begin
-	process (clk)
-	begin
-	if (rising_edge(clk)) then
-		presc_reg <= presc_reg + '1';		
-		if (presc_reg >= CLOCK_PRESCALER) then
-			presc_reg <= (others => '0');
-			sec_clk <= '1';
-			
-			min_presc <= min_presc + 1;
-			if(min_presc = 59) then
-				min_presc <= 0;
-				min_clk <= '1';
-			else
-				min_clk <= '0';
-			end if; -- min_presc = 59
-		else
-			sec_clk <= '0';
-		end if; -- presc_reg >= CLOCK_PRESCALER
-	end if; -- rising_edge(clk)
-	end process;
+	Presc_map :	Prescalers port map (clk_i 	   => clk, 
+									 clk_sec_o => sec_clk_sig,
+									 clk_min_o => min_clk_sig);
+										 
+	ClockCore_map : ClockCore port map (ClockCore_clk    => min_clk_sig,
+										ClockCore_time_o => cur_time);
 	
 	FourDigitDisplay_map : FourDigitDisplay port map (FourDigitDisplay_bcd => cur_time,
 													  FourDigitDisplay_segm(6 downto 0)   => segm_0,
 													  FourDigitDisplay_segm(13 downto 7)  => segm_1,
 													  FourDigitDisplay_segm(20 downto 14) => segm_2,
-													  FourDigitDisplay_segm(27 downto 21) => segm_3);
-	
-	ClockCore_map : ClockCore port map (ClockCore_clk    => min_clk,
-										ClockCore_time_o => cur_time);
-	
-	
---	segm_0_in <= segm_0_cntr;
---	u1: bcd_7seg port map(bin => segm_0_in, segm => segm_0_out);
---	segm_0 <= segm_0_out;
-	
-	
+													  FourDigitDisplay_segm(27 downto 21) => segm_3);	
 end rtl;
 
---
+++ Clock/Prescalers.vhd
@@ -0,0 +1,53 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+use work.Math_PACK.all;
+
+
+entity Prescalers is
+	port(
+		clk_i		:	in std_logic;
+		clk_sec_o	:	out std_logic;	
+		clk_min_o	:	out std_logic
+	);
+end Prescalers;
+
+
+architecture Behavioral of Prescalers is
+
+	constant CLOCK_PRESCALER 	: natural := (50000000);
+--	signal 	 sec_pr_reg			: std_logic_vector(f_log2(CLOCK_PRESCALER)-1 downto 0);
+	signal 	 sec_pr_reg			: integer range 0 to CLOCK_PRESCALER := 0;
+	signal   min_pr_reg			: integer range 0 to 60 := 0;
+
+	signal	 sec_clk			: std_logic := '0';
+	signal	 min_clk			: std_logic := '0';
+
+begin
+	process(clk_i)
+	begin
+		if(rising_edge(clk_i)) then
+--			sec_pr_reg <= sec_pr_reg + '1';		
+			sec_pr_reg <= sec_pr_reg + 1;		
+
+			if (sec_pr_reg >= CLOCK_PRESCALER) then
+--				sec_pr_reg <= (others => '0');
+				sec_pr_reg <= 0;
+				sec_clk <= '1';
+				
+				min_pr_reg <= min_pr_reg + 1;
+				if(min_pr_reg = 59) then
+					min_pr_reg <= 0;
+					min_clk <= '1';
+				else
+					min_clk <= '0';
+				end if; -- min_presc = 59
+			else
+				sec_clk <= '0';
+			end if; -- presc_reg >= CLOCK_PRESCALER					
+		end if; -- rising_edge(ClockCore_clk)
+	end process;
+	
+end Behavioral;
