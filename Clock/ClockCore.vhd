8a447bcd0d24e1508ea07afd82aba639e43664f2
Konstantin K <k.korzhanevskii@g.nsu.ru>
2023-10-25 09:01:48 +0700

    FSM interface

--- Clock/ClockCore.vhd
+++ Clock/ClockCore.vhd
@@ -6,9 +6,9 @@ use ieee.numeric_std.all;
 entity ClockCore is
 	port(
 		ClockCore_clk			:	in  std_logic;
---		ClockCore_up			:	in  std_logic;
---		ClockCore_down			:	in  std_logic;
-		ClockCore_time_o		:	out std_logic_vector(15 downto 0)
+		ClockCore_up			:	in  std_logic;
+		ClockCore_down			:	in  std_logic;
+		ClockCore_time_out		:	out std_logic_vector(15 downto 0)
 	);
 end ClockCore;
 
@@ -24,34 +24,57 @@ begin
 	process(ClockCore_clk)
 	begin
 		if(rising_edge(ClockCore_clk)) then
-		    m1 <= m1 + 1;
---			ClockCore_time_o(3 downto 0) <= std_logic_vector(to_unsigned(m1, 4));
-			
-			if m1 = 9 then
-				m10 <= m10 + 1;
-				m1 <= 0;
---				ClockCore_time_o(7 downto 4) <= std_logic_vector(to_unsigned(m2, 4));
-			
-				if m10 = 5 and m1 = 9 then
-					h1 	<= h1 + 1;
-					m1 	<= 0;
-					m10 <= 0;
+			if(ClockCore_up = '1') then			
+				m1 <= m1 + 1;
+				
+				if m1 = 9 then
+					m10 <= m10 + 1;
+					m1 <= 0;
+				
+					if m10 = 5 then
+						h1 	<= h1 + 1;
+						m10 <= 0;
+						m1 	<= 0;
 
---					ClockCore_time_o(11 downto 8) <= std_logic_vector(to_unsigned(h1, 4));
-					if h1 = 9 then
-					  h10 <= h10 + 1;
-					  h1  <= 0;
-					  
---					  ClockCore_time_o(15 downto 12)<= to_bitvector(std_logic_vector(to_unsigned(h2, 4)));
-					  
-					end if; -- h1
-					
-					if h10 = 2 and h1 = 4 then
-						 h10 <= 0;
-						 h1 <= 0;
-					end if; -- h10 h1 == 24
-				end if; -- m10
-			end if; -- m1
+						if h1 = 9 then
+						  h10 <= h10 + 1;
+						  h1  <= 0;						  
+						end if; -- h1
+						
+						if h10 = 2 and h1 = 3 then
+							 h10 <= 0;
+							 h1 <= 0;
+						end if; -- h10 h1 == 23
+					end if; -- m10
+				end if; -- m1
+			end if; -- ClockCore_up = '1'
+			
+		
+			if(ClockCore_down = '1') then			
+				m1 <= m1 - 1;
+				
+				if m1 = 0 then
+					m10 <= m10 - 1;
+					m1 <= 9;
+				
+					if m10 = 0 then
+						h1 	<= h1 - 1;
+						m10 <= 5;
+						m1 	<= 9;
+
+						if h1 = 0 then
+						  h10 <= h10 - 1;
+						  h1  <= 9;						  
+						end if; -- h1
+						
+						if h10 = 0 and h1 = 0 then
+							 h10 <= 2;
+							 h1 <= 3;
+						end if; -- h10 h1 == 24
+					end if; -- m10
+				end if; -- m1
+			end if; -- ClockCore_up = '1'
+			
 		end if; -- rising_edge(ClockCore_clk)
 	end process;
 	
--- Clock/FSM.vhd
+++ Clock/FSM.vhd
@@ -5,11 +5,36 @@ use ieee.numeric_std.all;
 
 entity FSM is
 	port(
-		clk			:	in  std_logic;
-		Up			: 	in  std_logic;
-		Down 		:	in  std_logic;
-		AlarmON		:	in  std_logic;
-		
-		data_out	:	out std_logic_vector(15 downto 0)
+		clk				:	in std_logic;
+		clk_1Hz			:	in std_logic;
+		clk_2Hz			:	in std_logic;
+		clk_10Hz		:	in std_logic;
+
+		Up_btn			: 	in std_logic;
+		Down_btn 		:	in std_logic;
+		Set_btn			:	in std_logic;	
+
+		FSM_sw  		: 	in std_logic;		
+		Alarm_sw		:	in std_logic;
+	
+		alarm_out		:	in std_logic;
+		alarm_set_out	:	in std_logic;
+		data_set_out	:	in std_logic;
+		data_out		:	out std_logic_vector(15 downto 0)
 	);
 end FSM;
+
+
+
+architecture Behavioral of ClockCore is
+
+	type State_t is (Tim, Tim_s, Alarm_s);
+	signal State : State_t := Tim;
+
+	
+
+begin
+
+
+
+end Behavioral;
