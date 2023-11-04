fd98e0470a48cb372479a3c4a008ffd5e771637a
Konstantin K <k.korzhanevskii@g.nsu.ru>
2023-11-04 14:39:15 +0700

    Uart interface

---
+++ UART_example/UART.vhd
@@ -0,0 +1,39 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+--use work.Math_PACK.all;
+
+entity UART is
+	generic (
+		constant Word_Width			: natural; 	-- Data width
+		constant System_Clock_Speed	: natural; 	-- 100MHz
+		constant Bit_Rate_value		: natural 	-- 1MHz
+	);	
+	port 
+	(
+		clk_i 	: in std_logic;
+		data_i	: in std_logic;
+		IRQ_o	: out std_logic;
+		data_o	: out std_logic_vector(Word_Width-1 downto 0)
+	);
+end entity;
+
+
+architecture Behavioral of UART is
+	
+	constant Oversampling_Ratio : natural := 4;		-- UART clk will be 4*Bit_Rate_Value (use Nyquist-Shannon theorem)
+	
+	constant Nyquist_Sampling_Speed 			: natural := Bit_Rate_Value*Oversampling_Ratio; 
+	constant System_to_Nyquist_Prescaler_Module : natural := System_Clock_Speed/Nyquist_Sampling_Speed;
+	-- start bit + data + parity bit + stop bit => Word_Width+3
+	constant Bit_Number_to_be_Processed			: natural := Word_Width+3;
+	constant Relative_Phase 					: natural := Oversampling_Ratio/2;	-- To determine the middle of start bit
+--	constant Sample_Number_Counter_Size 		: natural := f_log2(Bit_Number_to_be_Processed*Oversampling_Ratio);
+	
+	
+begin 
+
+
+end Behavioral;
--- UART_example/UART_example.vhd
+++ UART_example/UART_example.vhd
@@ -4,15 +4,29 @@ use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
 
 entity UART_example is
+	generic (
+		constant Data_Width			: natural := 8; 	-- Data width
+		constant System_Clock		: natural := 100; 	-- 100MHz
+		constant Bit_Rate		 	: natural := 1	-- 1MHz
+	);
 	port 
 	(
-		clk : in std_logic
+		clk 			: in  std_logic;
+		Input_Data_Line : in  std_logic;
+		IRQ				: out std_logic;
+		Output_Data_Bus	: out std_logic_vector(Data_Width-1 downto 0)
 	);
 end entity;
+
+
 
 architecture rtl of UART_example is
 	signal system_clk : std_logic;
 	
+	
+	--------------------------------------
+
+
 	component Main_PLL is
 	port
 	(
@@ -21,21 +35,53 @@ architecture rtl of UART_example is
 	);
 	end component;
 	
-begin
-
-	PLL_map: Main_PLL port map 
+	component UART is
+	generic (
+		constant Word_Width			: natural;
+		constant System_Clock_Speed	: natural;
+		constant Bit_Rate_value		: natural
+	);
+	port
 	(
-		inclk0 => clk,
-		c0 => system_clk
-	); 
+		clk_i 	: in std_logic;
+		data_i	: in std_logic;
+		IRQ_o	: out std_logic;
+		data_o	: out std_logic_vector(Data_Width-1 downto 0)
+	);
+	end component;
+
+
+begin
+	PLL_map: Main_PLL 
+		port map 
+		(
+			inclk0 => clk,
+			c0 => system_clk
+		); 
+
+		
+	UART_map: UART
+		generic map (
+			Word_Width 			=> Data_Width,
+			System_Clock_Speed 	=> System_Clock,
+			Bit_Rate_Value		=> Bit_Rate 
+			
+		)
+		port map 
+		(
+			clk_i	=> system_clk,
+			data_i	=> Input_Data_Line,
+			IRQ_o	=> IRQ,
+			data_o	=> Output_Data_Bus
+		); 
 	
-------------------------------------------	
+
+	--------------------------------------	
 	
 	process(system_clk)
 	begin 
 	
 	end process;
-
 
 
 end rtl;
