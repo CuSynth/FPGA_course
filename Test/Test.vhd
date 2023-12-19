52d08c25708fcc354dbf59f24f70377df06307a4
Konstantin K <k.korzhanevskii@g.nsu.ru>
2023-12-19 15:28:25 +0700

    Mem readout test implemented

--- Test/Test.vhd
+++ Test/Test.vhd
@@ -3,6 +3,9 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use work.all;
 
 entity Test is
 
@@ -14,32 +17,50 @@ entity Test is
 	port 
 	(
 		clk		: in std_logic;
-		s		: in std_logic;
-		r		: in std_logic;
+		en		: in std_logic;
 		q		: out std_logic
 	);
 
 end entity;
 
 architecture rtl of Test is
-	signal RS	: std_logic;
+	signal cnt	: std_logic_vector(5 downto 0);
+	signal sig  : std_logic_vector(0 downto 0);
 
+	
+	COMPONENT Test_mem_32_x_1bit
+	PORT (
+		address		: IN STD_LOGIC_VECTOR (4 DOWNTO 0);
+		clock		: IN STD_LOGIC ;
+		data		: IN STD_LOGIC_VECTOR (0 DOWNTO 0);
+		wren		: IN STD_LOGIC ;
+		q			: OUT STD_LOGIC_VECTOR (0 DOWNTO 0)
+	);
+	END COMPONENT;
 begin
+		RAM1024_inst : Test_mem_32_x_1bit PORT MAP (
+		address	 => cnt(4 downto 0), 
+		clock	 => clk,
+		data	 => (others => '0'),
+		wren	 => '0',
+		q	 	 => sig
+	);
+
 
 	process (clk)
 	begin
 		if (rising_edge(clk)) then
-
-			if (s = '1') then
-				RS <= '1';
+			if(en = '1') then
+				cnt <= cnt + '1';
+				q <= sig(0);
+			else
+				q <= '0';
 			end if;
-
-			if (r = '1') then
-				RS <= '0';
+			if(cnt=31) then
+				cnt <= (others=>'0');
 			end if;
-			
 		end if;
 	end process;
 
-	q <= RS;
+
 end rtl;
